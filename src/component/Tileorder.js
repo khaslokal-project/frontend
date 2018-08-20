111a5723cccc0d6e2ceb10c3bd2be177b8398545
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-21 04:37:47 +0700

    Refactor code

--- src/component/Tileorder.js
+++ src/component/Tileorder.js
@@ -4,19 +4,42 @@ import Button from '@material-ui/core/Button';
 import { List, Typography } from '@material-ui/core';
 
 
-export const mailFolderListItemsRight = (
-    <div>
-        <List>
-            <ListItem>
-                <Typography>
-        Keranjang belanja Sista masih kosong. Ayo diborong Sis, sebelum kehabisan.                       
-                </Typography>
-            </ListItem>
-            <ListItem>
-                <Button style={{marginRight: '10px', width: '100%'}} variant="contained" color="secondary" >
-        Lanjut Belanja
-                </Button>
-            </ListItem>
-        </List>   
-    </div>
-);
+const mailFolderListItemsRight = ({data,id}) =>{
+    if(id===''){
+        return (
+            <div>
+                <List>
+                    <ListItem>
+                        <Typography>
+                Keranjang belanja Sista masih kosong. Ayo diborong Sis, sebelum kehabisan.                       
+                        </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Button style={{marginRight: '10px', width: '100%'}} variant="contained" color="secondary" >
+                Lanjut Belanja
+                        </Button>
+                    </ListItem>
+                </List>   
+            </div>
+        );
+    }else{
+        return(
+            <div>
+                <List>
+                    <ListItem>
+                        <Typography>Nama item : {data.name}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Harga item : {data.price}</Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography>Merk item : {data.brand}</Typography>
+                    </ListItem>
+                </List>
+            </div>
+        );
+    }
+    
+};
+ 
+export default mailFolderListItemsRight;
