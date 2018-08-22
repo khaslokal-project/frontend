df77e930895ebe67ce4eedb2647092a602298016
amirul-inc <it.amirulfatta@gmail.com>
2018-08-22 23:50:13 +0700

    refactor router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import AppProvider from './component/AppProvider';
 import './App.css';
-import Navbar from './component/Navbar';
+import NavbarBeforeLogin from './component/Navbar';
+import NavbarAfterLogin from './component/NavbarAfterlogin';
 
 class App extends Component {
 
     render() {
         return (
-            <AppProvider>
-                <div className="App" style={{paddingTop:'4.7rem'}} justify="center">
-                    <Navbar />
-                </div>
-            </AppProvider>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={NavbarBeforeLogin}/>
+                    <Route exact path="/home" component={NavbarAfterLogin}/>
+                </Switch>
+            </Router>
+        // <AppProvider>     <div className="App" style={{paddingTop:'4.7rem'}}
+        // justify="center">         <Navbar />     </div> </AppProvider>
         );
     }
 }
