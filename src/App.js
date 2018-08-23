320e2ba09534e33c54cd42afff8f69723fa8f0bd
fabilqis <fadillah.bilqis@gmail.com>
2018-08-23 12:46:16 +0700

    Delete hashrouter on appjs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch, HashRouter } from 'react-router-dom';
 import AppProvider from './component/AppProvider';
 import './App.css';
-import {HashRouter, Switch, Route} from 'react-router-dom'
 import NavbarBeforeLogin from './component/Navbar';
 import NavbarAfterLogin from './component/NavbarAfterlogin';
 class App extends Component {
@@ -13,12 +12,12 @@ class App extends Component {
             <Router>
                 <AppProvider>     
                     <div className="App" style={{paddingTop:'4.7rem'}} justify="center">         
-                        <HashRouter>
+                        
                             <Switch>
                                 <Route exact path="/" component={NavbarBeforeLogin}/>
                                 <Route exact path="/home" component={NavbarAfterLogin}/>
                             </Switch>
-                        </HashRouter>   
+                          
                     </div> 
                 </AppProvider>
             </Router>
--- src/pages/Masuk.js
+++ src/pages/Masuk.js
@@ -31,7 +31,7 @@ class Masuk extends Component {
                 if (res.data.errors) {
                     return this.setState({ valerrors: res.data.errors });
                 }
-                return (window.location = '/');
+                return (window.location = '/home');
             });
     }
     render() {
